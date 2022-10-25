7bd3ccba6a053a858cf14521d73c765a6402bbbb
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2022-10-25 17:27:29 +0200

    added time

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import Handler from "./core/handler.js";
+import schedual from "node-schedule";
 
-await (await new Handler().load().check()).notify();
+const Time_config = new schedual.RecurrenceRule();
+
+Time_config.dayOfWeek = [0, new schedual.Range(0, 6)];
+Time_config.hour = [0, new schedual.Range(0, 23)];
+Time_config.minute = 30;
+
+var Job = schedual.scheduleJob(Time_config, async () => {
+  await (await new Handler().load().check()).notify();
+});
